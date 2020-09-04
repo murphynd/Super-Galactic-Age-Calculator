2bf6f8bdc93d6205900f3b6d991e8947851c879f
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-09-04 09:47:43 -0700

    test 2 for class planet passes

--- src/js/Planet.js
+++ src/js/Planet.js
@@ -1,8 +1,10 @@
 export default class Planet {
-  constructor(planet, earthYears, humanYears) {
+  constructor(planet, earthYears) {
     this.planet = planet;
     this.earthYears = earthYears;
-    this.humanYears = humanYears;
+    this.humanYears = 0;
   }
-  age() {}
+  age(num) {
+    this.humanYears = num *= this.earthYears;
+  }
 }
